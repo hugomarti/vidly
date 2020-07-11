39263ad065320e7e7c1c84251735c25aec1bd26c
hugomarti <hgmarti@gmail.com>
2020-07-11 12:59:38 +0800

    form validation implementation with Joi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
-import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
+
+import Navbar from "./components/navBar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm.jsx";
+import LoginForm from "./components/loginForm";
+
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <Navbar />
+      <main className="container">
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
